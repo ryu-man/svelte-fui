fea525c9b72604d66f565d7e4eeddab60b3a0d5f
Abdelhalim Riache <rioman19@gmail.com>
2023-05-04 10:19:42 +0100

    feat: init `tooltip` module

---
+++ src/lib/tooltip/Tooltip.stories.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import type { ArgTypes } from '@storybook/svelte';
+	import { App, Button, Tooltip } from 'svelte-fluentui';
+
+	const argDefaults = {
+		disabled: false,
+		required: false,
+		size: 'md',
+		weight: 'regular'
+	};
+
+	const argTypes = {
+		disabled: {
+			type: 'boolean',
+			defaultValue: false,
+			control: {
+				type: 'boolean'
+			}
+		},
+		required: {
+			type: 'boolean',
+			defaultValue: false,
+			control: {
+				type: 'boolean'
+			}
+		},
+		size: {
+			type: 'string',
+			defaultValue: 'md',
+			options: ['sm', 'md', 'lg'],
+			control: {
+				type: 'select'
+			}
+		},
+		weight: {
+			type: 'string',
+			defaultValue: 'regular',
+			options: ['regular', 'semibold'],
+			control: {
+				type: 'select'
+			}
+		}
+	} satisfies ArgTypes;
+</script>
+
+<Meta title="Components/Tooltip" component={Tooltip} />
+
+<Story id="fui_tooltip" name="Tooltip" let:args>
+	<App>
+		<div class="flex justify-center items-center w-full h-full">
+			<Tooltip {...args}>
+				<Button>Hello Svelte land</Button>
+
+				<span slot="content">Hello from the other side! [slotted]</span>
+			</Tooltip>
+		</div>
+	</App>
+</Story>
