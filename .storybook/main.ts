272c542c1b819e465d0ba29b954db6490a5fd721
Abdelhalim Riache <rioman19@gmail.com>
2023-05-06 12:23:43 +0100

    chore: reconfigure storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/sveltekit';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-svelte-csf', '@storybook/addon-mdx-gfm'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-svelte-csf'],
   framework: {
     name: '@storybook/sveltekit',
     options: {
