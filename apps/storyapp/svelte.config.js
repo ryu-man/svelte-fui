6569930e88768d10b88088f514a74e94be9292f8
Abdelhalim Riache <rioman19@gmail.com>
2023-05-24 22:58:58 +0100

    set aliases

--- apps/storyapp/svelte.config.js
+++ apps/storyapp/svelte.config.js
@@ -13,8 +13,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		alias:{
-			'@svelte-fui/core': path.resolve('../../packages/core/src')
+		alias: {
+			'@svelte-fui/core': path.resolve('../../packages/core/src'),
+			'@svelte-fui/tailwindcss': path.resolve('../../packages/tailwindcss/src'),
+			'@svelte-fui/theme': path.resolve('../../packages/theme/src'),
+			'@svelte-fui/themes': path.resolve('../../packages/themes/src'),
 		}
 	}
 };
