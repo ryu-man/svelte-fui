97aa8851d3d100dd0e469a761ca6b0d0d71a864c
Abdelhalim Riache <rioman19@gmail.com>
2023-05-24 08:51:29 +0100

    optimize deps

--- apps/storyapp/vite.config.ts
+++ apps/storyapp/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps:{
+		include:[
+			"@svelte-fui/core",
+			"@svelte-fui/tailwind",
+			"@svelte-fui/theme",
+			"@svelte-fui/themes",
+			"@svelte-fui/icons"
+		]
+	}
 });
