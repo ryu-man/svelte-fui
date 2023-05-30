5bc430adb351e3d4d21be4a6ddbe4302b0ffa210
Abdelhalim Riache <rioman19@gmail.com>
2023-05-30 14:09:09 +0100

    fix HMR issues

--- apps/storyapp/vite.config.ts
+++ apps/storyapp/vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps:{
-		include:[
-			"@svelte-fui/core",
-			"@svelte-fui/tailwind",
-			"@svelte-fui/theme",
-			"@svelte-fui/themes",
-			"@svelte-fui/icons"
-		]
+	optimizeDeps: {
+		exclude: [
+			'@svelte-fui/core',
+			'@svelte-fui/tailwind',
+			'@svelte-fui/theme',
+			'@svelte-fui/themes'
+		],
+		include: ['@svelte-fui/icons']
 	}
 });
