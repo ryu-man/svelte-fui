0f6c1cadfcea8fd24ed69b144fe14d7fc9de8540
abdelhalim <rioman19@gmail.com>
2023-07-24 16:23:52 +0100

    deprecate `tailwindcssConfig` in favor of `fuiPreset`

--- apps/storyapp/tailwind.config.js
+++ apps/storyapp/tailwind.config.js
@@ -1,8 +1,8 @@
-import { tailwindcssConfig } from '../../packages/tailwindcss/src'
+import { fuiPreset } from '../../packages/tailwindcss/src'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [tailwindcssConfig],
+    presets: [fuiPreset],
     content: ['./src/**/*.{html,js,svelte, stories.svelte, ts}', '../../packages/core/src/**/*.{html,js,svelte, stories.svelte, ts}'],
 
 }
--- packages/tailwindcss/src/index.ts
+++ packages/tailwindcss/src/index.ts
@@ -1,1 +1,5 @@
+/**
+ * @deprecated Use `fuiPreset` instead.
+ */
 export { default as tailwindcssConfig } from './tailwind.config';
+export { default as fuiPreset } from './tailwind.config';
