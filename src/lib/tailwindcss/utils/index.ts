0322d98eb9a3c8cea28be2ef83ad4c502e129354
abdelhalim <rioman19@gmail.com>
2023-11-24 18:43:52 +0100

    remove not used files

--- src/lib/tailwindcss/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export function tailwindColorify(name: string, colors: Record<string, string>) {
-	return Object.entries(colors).reduce((acc, [key, value]) => {
-		acc[[name, key].join('-')] = value;
-
-		return acc;
-	}, {});
-}
