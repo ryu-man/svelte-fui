2570b81c7a5deacea8366ca078fd222f0ef3b74f
abdelhalim <rioman19@gmail.com>
2024-05-20 08:12:45 -0400

    chore: update exports

--- packages/core/src/field/index.ts
+++ packages/core/src/field/index.ts
@@ -1,2 +1,3 @@
 export { default as Field } from './field.svelte';
 export { default as ValidationMessage } from './field-message.svelte'
+export { default as FieldMessage } from './field-message.svelte'
