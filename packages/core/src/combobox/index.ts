9b505d701a8c1b7424c3de20aa86edebbabbb737
abdelhalim <rioman19@gmail.com>
2024-05-22 15:09:01 -0400

    delete unused file

--- packages/core/src/combobox/index.ts
+++ packages/core/src/combobox/index.ts
@@ -2,4 +2,3 @@ export { default as Combobox } from './combobox-root.svelte';
 export { default as ComboboxListbox } from './listbox.svelte';
 export { default as Listbox } from './listbox.svelte';
 export { default as Option } from './combobox-option.svelte';
-export { default as Dropdown } from './dropdown-root.svelte';
