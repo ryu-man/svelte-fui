5fe5611b93a69be3ecf65e04879767f316eaf0a8
abdelhalim <rioman19@gmail.com>
2024-12-21 22:41:25 +0100

    make it straightforward

--- packages/core/src/lib/radio/context.ts
+++ packages/core/src/lib/radio/context.ts
@@ -1,5 +1,5 @@
 import { getContext, setContext } from 'svelte';
-import { type Writable, writable } from 'svelte/store';
+import { type Writable } from 'svelte/store';
 import type { Layout } from './types';
 
 const key = 'fui_radio_group_context';
@@ -12,30 +12,10 @@ export type RadioGroupContext = {
 	layout$: Writable<Layout>;
 };
 
-const builders: Record<keyof RadioGroupContext, () => any> = {
-	disabled$: () => writable(false),
-	required$: () => writable(false),
-	name$: () => writable(undefined),
-	value$: () => writable(undefined),
-	layout$: () => writable('vertical')
-};
-
-function getDefaultContext(context: Partial<RadioGroupContext> = {}) {
-	const keys: Set<keyof RadioGroupContext> = new Set(['disabled$', 'required$', 'value$', 'name$']);
-
-	Object.keys(context).forEach((key) => keys.delete(key));
-
-	for (const key of keys) {
-		context[key] = builders[key]();
-	}
-
-	return context as RadioGroupContext;
-}
-
 export function getRadioGroupContext() {
-	return (getContext(key) || getDefaultContext()) as RadioGroupContext;
+	return getContext(key) as RadioGroupContext;
 }
 
-export function setRadioGroupContext(context: Partial<RadioGroupContext> = {}): RadioGroupContext {
-	return setContext(key, getDefaultContext(context));
+export function setRadioGroupContext(context: RadioGroupContext): RadioGroupContext {
+	return setContext(key, context);
 }
