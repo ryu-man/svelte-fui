9de010f7238ba620c28c0166d4b84a5da597246f
abdelhalim <rioman19@gmail.com>
2025-03-22 21:15:28 -0400

    make props optional

--- packages/core/src/lib/app/types.ts
+++ packages/core/src/lib/app/types.ts
@@ -3,8 +3,8 @@ import type { Snippet } from 'svelte';
 import type { FluentRootContext } from './root-context';
 
 export type RootProps = {
-	theme: Theme;
-	screens: Record<string, string>;
+	theme?: Theme;
+	screens?: Record<string, string>;
 	children?: Snippet<
 		[
 			{
