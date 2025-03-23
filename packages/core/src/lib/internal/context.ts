adc6fd70d33f71225f5565aebdabc7ab03d3b14b
abdelhalim <rioman19@gmail.com>
2025-03-22 21:15:10 -0400

    update context type and add helper function to create state for svelte 5

--- packages/core/src/lib/internal/context.ts
+++ packages/core/src/lib/internal/context.ts
@@ -2,11 +2,17 @@ import { getContext, setContext } from 'svelte';
 
 const FLUENT_CONTEXT_KEY = '@fui/context';
 
-export type FluentContext = {
+export type FluentContext<S> = {
 	id: string;
 	type: string;
-	parent?: () => FluentContext | undefined;
+	parent: () => FluentContext<S> | undefined;
+	update: (fn: (state: S) => void) => void;
+	readonly state: S;
 };
+
+export function getFluentContextPath(...segments: string[]){
+	return [FLUENT_CONTEXT_KEY, ...segments].filter(Boolean).join('/')
+}
 
 export function getFluentContext<T>(...segments: (string | undefined)[]) {
 	const key = [FLUENT_CONTEXT_KEY, ...segments].filter(Boolean).join('/');
@@ -29,4 +35,28 @@ export function setSharedContext<T>(context: T, id = '', ...segments: string[]) 
 
 export function mergeContext<T>(...contexts: (T | undefined)[]) {
 	return contexts.filter(Boolean).reduce<T>((acc, val) => ({ ...acc, ...val }), {} as T);
+}
+
+export function defineProperty<T extends object, R>(
+	base: T,
+	property: keyof T,
+	get: () => R,
+	set?: (value: R) => void
+) {
+	return Object.defineProperty(base, property, {
+		get: get,
+		set: set
+	});
+}
+
+export type StateDefiner<T extends object> = (base: T) => T;
+
+export function defineState<T extends object>(definers: StateDefiner<T>[], base: T = {} as T) {
+	let outcome = { ...base };
+
+	for (const definer of definers) {
+		outcome = definer(outcome);
+	}
+
+	return outcome;
 }
