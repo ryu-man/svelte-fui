195a6e814ba7a594d211a927787150aa229f1b00
abdelhalim <rioman19@gmail.com>
2025-04-03 19:48:40 -0400

    use menu list and indicator

--- packages/core/src/lib/dropdown/components.ts
+++ packages/core/src/lib/dropdown/components.ts
@@ -1,6 +1,5 @@
 export { default as Checkbox } from './dropdown-checkbox.svelte';
-export { default as Indicator } from './dropdown-indicator.svelte';
 export { default as Item } from './dropdown-item.svelte';
-export { default as List } from './dropdown-list.svelte';
+export { List, Indicator } from '../menu/components';
 export { default as Root } from './dropdown-root.svelte';
 export { default as Trigger } from './dropdown-trigger.svelte';
