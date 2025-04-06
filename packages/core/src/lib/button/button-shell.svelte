4e99131b1a864d7ffeaf99d198072a7b93e0ca3b
abdelhalim <rioman19@gmail.com>
2025-04-05 22:15:17 -0400

    create button shell component to reuse button style

---
+++ packages/core/src/lib/button/button-shell.svelte
@@ -0,0 +1,231 @@
+<script lang="ts">
+	import { classnames } from '@svelte-fui/core/internal';
+	import type { ButtonProps } from './types';
+
+	let {
+		element = $bindable(),
+		class: klass,
+		shape,
+		appearance,
+		size = 'md',
+		icon,
+		as = 'button',
+		children = undefined,
+		...restProps
+	}: ButtonProps & { as: string } = $props();
+</script>
+
+<svelte:element
+	this={as}
+	bind:this={element}
+	class={classnames(
+		'fui-button-shell',
+		'px-m gap-sNudge font-base text-base-300 min-h-xxl leading-base-300 inline-flex w-auto justify-center rounded-md py-[5px] font-semibold border-thin m-0 box-border select-none items-center align-middle no-underline outline-none',
+		appearance,
+		shape,
+		size,
+		icon && 'h-xxxl aspect-square',
+		size === 'sm' && 'px-s text-base-200 font-regular leading-base-200 min-h-[24px] py-[3px]',
+		size === 'lg' && 'px-m text-base-400 leading-base-400 min-h-[40px] py-[8px]',
+		size === 'sm' && icon && 'h-[24px] px-[1px] py-[1px]',
+		size === 'md' && icon && 'text-base-500 aspect-square px-[5px] py-[5px]',
+		size === 'lg' && icon && 'h-[40px] px-[7px] py-[7px]',
+		shape === 'circular' && 'rounded-full',
+		shape === 'square' && 'rounded-none',
+		klass
+	)}
+	{...restProps}
+>
+	{@render children?.()}
+</svelte:element>
+
+<style lang="postcss">
+	.fui-button-shell {
+		--min-height: calc(theme(lineHeight.base-300) + 5px * 2 + theme(borderWidth.thin) * 2);
+		--fui-icon-size: theme(lineHeight.base-300);
+
+		background-color: var(--fui-colorNeutralBackground1);
+		border-color: var(--fui-colorNeutralStroke1);
+		border-style: solid;
+		color: var(--fui-colorNeutralForeground1);
+
+		text-decoration: none;
+		outline: none;
+		cursor: default;
+
+		transition-duration: 0.1s;
+		transition-property: all;
+
+		&:hover {
+			background-color: var(--fui-colorNeutralBackground1Hover);
+			border-color: var(--fui-colorNeutralStroke1Hover);
+			color: var(--fui-colorNeutralForeground1Hover);
+			cursor: pointer;
+			/* backgroundColor: tokens.colorNeutralBackground1Hover,
+				borderColor: tokens.colorNeutralStroke1Hover,
+				color: tokens.colorNeutralForeground1Hover,
+
+				cursor: 'pointer',
+
+				[`& .${iconFilledClassName}`]: {
+				display: 'inline',
+				},
+				[`& .${iconRegularClassName}`]: {
+				display: 'none',
+				}, */
+		}
+
+		&:hover:active {
+			background-color: var(--fui-colorNeutralBackground1Pressed);
+			border-color: var(--fui-colorNeutralStroke1Pressed);
+			color: var(--fui-colorNeutralForeground1Pressed);
+
+			/* ':hover:active': {
+				backgroundColor: tokens.colorNeutralBackground1Pressed,
+				borderColor: tokens.colorNeutralStroke1Pressed,
+				color: tokens.colorNeutralForeground1Pressed,
+
+				outlineStyle: 'none',
+
+				[`& .${iconFilledClassName}`]: {
+				display: 'inline',
+				},
+				[`& .${iconRegularClassName}`]: {
+				display: 'none',
+				},
+			}, */
+		}
+
+		&:focus-visible {
+			box-shadow: var(--fui-focus-stroke);
+		}
+	}
+
+	@media screen and (prefers-reduced-motion: reduce) {
+		.fds-button {
+			transition-duration: 0.01ms;
+		}
+	}
+
+	.fds-button.outline {
+		background-color: var(--fui-colorTransparentBackground);
+
+		&:hover {
+			background-color: var(--fui-colorTransparentBackground);
+		}
+
+		&:hover:active {
+			background-color: var(--fui-colorTransparentBackground);
+		}
+	}
+
+	.fds-button.primary {
+		background-color: var(--fui-colorBrandBackground);
+		border-color: transparent;
+		color: var(--fui-colorNeutralForegroundOnBrand);
+
+		&:hover {
+			background-color: var(--fui-colorBrandBackgroundHover);
+			border-color: transparent;
+			color: var(--fui-colorNeutralForegroundOnBrand);
+		}
+
+		&:hover:active {
+			background-color: var(--fui-colorBrandBackgroundPressed);
+			border-color: transparent;
+			color: var(--fui-colorNeutralForegroundOnBrand);
+		}
+	}
+
+	.fds-button.subtle {
+		background-color: var(--fui-colorSubtleBackground);
+		border-color: transparent;
+		color: var(--fui-colorNeutralForeground2);
+
+		&:hover {
+			background-color: var(--fui-colorSubtleBackgroundHover);
+			border-color: transparent;
+			color: var(--fui-colorNeutralForeground2Hover);
+
+			/* [`& .${buttonClassNames.icon}`]: {
+        color: tokens.colorNeutralForeground2BrandHover,
+      }, */
+		}
+
+		&:hover:active {
+			background-color: var(--fui-colorSubtleBackgroundPressed);
+			border-color: transparent;
+			color: var(--fui-colorNeutralForeground2Pressed);
+
+			/* [`& .${buttonClassNames.icon}`]: {
+        color: tokens.colorNeutralForeground2BrandPressed,
+      }, */
+		}
+	}
+
+	.fds-button.transparent {
+		background-color: var(--fui-colorTransparentBackground);
+		border-color: transparent;
+		color: var(--fui-colorNeutralForeground2);
+
+		&:hover {
+			background-color: var(--fui-colorTransparentBackgroundHover);
+			border-color: transparent;
+			color: var(--fui-colorNeutralForeground2BrandHover);
+
+			/* [`& .${buttonClassNames.icon}`]: {
+        color: tokens.colorNeutralForeground2BrandHover,
+      }, */
+		}
+
+		&:hover:active {
+			background-color: var(--fui-colorTransparentBackgroundPressed);
+			border-color: transparent;
+			color: var(--fui-colorNeutralForeground2BrandPressed);
+
+			/* [`& .${buttonClassNames.icon}`]: {
+        color: tokens.colorNeutralForeground2BrandPressed,
+      }, */
+		}
+	}
+
+	.fds-button:disabled {
+		background-color: var(--fui-colorNeutralBackgroundDisabled);
+		border-color: var(--fui-colorNeutralStrokeDisabled);
+		color: var(--fui-colorNeutralForegroundDisabled);
+
+		cursor: not-allowed;
+
+		&:hover {
+			background-color: var(--fui-colorNeutralBackgroundDisabled);
+			border-color: var(--fui-colorNeutralStrokeDisabled);
+			color: var(--fui-colorNeutralForegroundDisabled);
+
+			cursor: not-allowed;
+			/* [`& .${iconFilledClassName}`]: {
+        display: 'none',
+      },
+      [`& .${iconRegularClassName}`]: {
+        display: 'inline',
+      }, */
+		}
+
+		&:hover:active {
+			background-color: var(--fui-colorNeutralBackgroundDisabled);
+			border-color: var(--fui-colorNeutralStrokeDisabled);
+			color: var(--fui-colorNeutralForegroundDisabled);
+
+			cursor: not-allowed;
+
+			/* [`& .${iconFilledClassName}`]: {
+        display: 'none',
+      },
+      [`& .${iconRegularClassName}`]: {
+        display: 'inline',
+      }, */
+		}
+	}
+
+	@media (forced-colors: active) {
+	}
+</style>
