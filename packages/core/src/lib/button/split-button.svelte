b24b5d1b5be6dd9d4f8639c92a7db75d26360445
abdelhalim <rioman19@gmail.com>
2025-04-05 22:15:39 -0400

    create split button

---
+++ packages/core/src/lib/button/split-button.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { classnames } from '../internal';
+	import { Menu } from '../menu';
+	import Button from './button.svelte';
+	import type { ButtonProps } from './types';
+
+	let {
+		element = $bindable(),
+		class: klass = '',
+		shape,
+		appearance,
+		size,
+		icon,
+		children,
+		onclick = undefined,
+		...restProps
+	}: ButtonProps = $props();
+</script>
+
+<button class="fui-split-btn flex items-center">
+	<Button
+		class={classnames('items-center flex gap-2', klass)}
+		{shape}
+		{appearance}
+		{size}
+		onclick={(ev) => {
+			ev.stopPropagation();
+			ev.preventDefault();
+
+			onclick?.(ev);
+		}}
+		{...restProps}
+	>
+		{@render children?.()}
+	</Button>
+
+	<Button bind:element class="px-0" {shape} {appearance} {size} icon>
+		<Menu.Indicator class="h-4" />
+	</Button>
+</button>
+
+<style>
+	.fui-split-btn :global(.fui-button:first-child) {
+		border-top-right-radius: 0 !important;
+		border-bottom-right-radius: 0 !important;
+	}
+
+	.fui-split-btn :global(.fui-button:last-child) {
+		border-top-left-radius: 0 !important;
+		border-bottom-left-radius: 0 !important;
+		border-left-width: 0 !important;
+	}
+</style>
