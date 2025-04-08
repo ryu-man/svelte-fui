f7c04c17e1546d5c5277556ca295e0b2e9465794
abdelhalim <rioman19@gmail.com>
2025-04-08 19:21:41 -0400

    export as esm instead of object

---
+++ packages/core/src/lib/popover/components.ts
@@ -0,0 +1,4 @@
+export { default as Indicator } from './popover-indicator.svelte';
+export { default as Overlay } from './popover-overlay.svelte';
+export { default as Trigger } from './popover-trigger.svelte';
+export { default as Root } from './popover-root.svelte';
--- packages/core/src/lib/popover/index.ts
+++ packages/core/src/lib/popover/index.ts
@@ -1,14 +1,4 @@
-import PopoverIndicator from './popover-indicator.svelte';
-import PopoverOverlay from './popover-overlay.svelte';
-import PopoverTrigger from './popover-trigger.svelte';
-import PopoverRoot from './popover-root.svelte';
-
 export * from './context';
 export * from './types';
 
-export const Popover = {
-	Root: PopoverRoot,
-	Overlay: PopoverOverlay,
-	Trigger: PopoverTrigger,
-	Indicator: PopoverIndicator
-};
+export * as Popover from './components';
