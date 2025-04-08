9df754b3228b43fa42b23dc8f1834328ed2dd49e
abdelhalim <rioman19@gmail.com>
2025-04-08 18:54:49 -0400

    set theme

--- packages/core/src/lib/icon/icon.stories.svelte
+++ packages/core/src/lib/icon/icon.stories.svelte
@@ -3,6 +3,8 @@
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import type { ArgTypes } from '@storybook/svelte';
 	import AlertRegularIcon from 'virtual:icons/fluent/alert-48-regular';
+	import { onMount } from 'svelte';
+	import { webDarkTheme, webLightTheme } from '@svelte-fui/themes';
 
 	const argTypes = {} satisfies ArgTypes;
 
@@ -16,11 +18,28 @@
 </script>
 
 <script>
+	let theme = $state(webLightTheme);
+
+	onMount(() => {
+		function handler(schemeMedia: MediaQueryListEvent) {
+			theme = schemeMedia.matches ? webLightTheme : webDarkTheme;
+		}
+
+		const schemeMedia = matchMedia('(prefers-color-scheme: light)');
+
+		schemeMedia.addEventListener('change', handler);
+
+		theme = schemeMedia.matches ? webLightTheme : webDarkTheme;
+
+		return () => {
+			schemeMedia.removeEventListener('change', handler);
+		};
+	});
 </script>
 
-<Story id="icon" name="Icon" args={defaultArgs}>
+<Story name="Icon" args={defaultArgs}>
 	{#snippet children(args)}
-		<FluentRoot>
+		<FluentRoot {theme}>
 			<div class="flex h-full w-full items-center justify-center">
 				<IconFui {...args} class="">
 					<AlertRegularIcon />
