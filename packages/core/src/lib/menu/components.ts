9317ccbfda8333076da7a71ab7f648ed39bd1666
abdelhalim <rioman19@gmail.com>
2025-04-08 18:13:52 -0400

    export list components

--- packages/core/src/lib/menu/components.ts
+++ packages/core/src/lib/menu/components.ts
@@ -1,7 +1,6 @@
 export { default as Indicator } from '../popover/popover-indicator.svelte';
-export { default as Divider } from './menu-divider.svelte';
-export { default as Group } from './menu-group.svelte';
 export { default as Item } from './menu-item.svelte';
 export { default as List } from './menu-list.svelte';
 export { default as Root } from './menu-root.svelte';
 export { default as Trigger } from './menu-trigger.svelte';
+export { Group, Title, Divider } from '../list/components';
