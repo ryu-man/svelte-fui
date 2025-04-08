38e8921cf09fed7d6f8bcfe696ca2877273ed7a9
abdelhalim <rioman19@gmail.com>
2025-04-08 18:09:15 -0400

    update story

--- packages/core/src/lib/popover/popover.stories.svelte
+++ packages/core/src/lib/popover/popover.stories.svelte
@@ -1,10 +1,10 @@
 <script module lang="ts">
-	import { FluentRoot, Link } from '@svelte-fui/core';
+	import { Button, FluentRoot} from '@svelte-fui/core';
 	import { Popover as PopoverModule } from '.';
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import type { ArgTypes } from '@storybook/svelte';
-	import { fly } from 'svelte/transition';
-	import { webLightTheme } from '@svelte-fui/themes';
+	import { webDarkTheme, webLightTheme } from '@svelte-fui/themes';
+	import { onMount } from 'svelte';
 
 	const arg_types = {} satisfies ArgTypes;
 
@@ -18,7 +18,23 @@
 
 <script lang="ts">
 	let open = $state(false);
-	let theme = webLightTheme;
+	let theme = $state(webLightTheme);
+
+	onMount(() => {
+		function handler(schemeMedia: MediaQueryListEvent) {
+			theme = schemeMedia.matches ? webLightTheme : webDarkTheme;
+		}
+
+		const schemeMedia = matchMedia('(prefers-color-scheme: light)');
+
+		schemeMedia.addEventListener('change', handler);
+
+		theme = schemeMedia.matches ? webLightTheme : webDarkTheme;
+
+		return () => {
+			schemeMedia.removeEventListener('change', handler);
+		};
+	});
 
 	function onclick() {
 		console.log(open);
@@ -28,42 +44,39 @@
 <Story name="Popover" args={default_args}>
 	{#snippet children(args)}
 		<FluentRoot class="p-4" {theme}>
-			<PopoverModule.Root bind:open>
-				<PopoverModule.Trigger
-					class="flex aspect-square h-12 items-center justify-center rounded-xl border border-neutral-200 bg-neutral-100"
-					as="button"
-					{onclick}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-					>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12Z"
-							fill="#3A3A3B"
-						/>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5Z"
-							fill="#3A3A3B"
-						/>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M10 19C10 17.8954 10.8954 17 12 17C13.1046 17 14 17.8954 14 19C14 20.1046 13.1046 21 12 21C10.8954 21 10 20.1046 10 19Z"
-							fill="#3A3A3B"
-						/>
-					</svg>
-				</PopoverModule.Trigger>
+			<div class="flex justify-center items-center w-full h-full">
+				<PopoverModule.Root bind:open>
+					<PopoverModule.Trigger shell={Button} icon {onclick}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-4 text-neutral-foreground-1"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12Z"
+								fill="currentColor"
+							/>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5Z"
+								fill="currentColor"
+							/>
+							<path
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M10 19C10 17.8954 10.8954 17 12 17C13.1046 17 14 17.8954 14 19C14 20.1046 13.1046 21 12 21C10.8954 21 10 20.1046 10 19Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</PopoverModule.Trigger>
 
-				<PopoverModule.Overlay>Hello World!</PopoverModule.Overlay>
-			</PopoverModule.Root>
+					<PopoverModule.Overlay>Hello World!</PopoverModule.Overlay>
+				</PopoverModule.Root>
+			</div>
 		</FluentRoot>
 	{/snippet}
 </Story>
